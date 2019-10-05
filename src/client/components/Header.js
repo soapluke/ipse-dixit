7858075161f93ed94dcf2ac67dca7e4cf85ccb73
soapluke <lukaslindgren@gmail.com>
2019-10-05 16:21:26 +0200

    setting up the header

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -4,11 +4,18 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="header">
-            <div className="header__content">
-                <Link className="header__title" to="/dashboard">
-                    <h1>Ipse Dixit</h1>
-                </Link>
-            </div>  
+            <div className="content-container">
+                <div className="header__content">
+                    <Link className="header__title" to="/dashboard">
+                        <h1 className="header__logo">Ipse Dixit</h1>
+                    </Link>
+                    <div className="header__buttons">
+                        <button>Login</button>
+                        <button>Register</button>
+                    </div>
+                    
+                </div> 
+            </div>
         </header>
     );
 }
